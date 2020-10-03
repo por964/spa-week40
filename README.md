e5e1dc5be155a85dd52e90904dc0e67bc1e5b7fa
Claes <claesvonh@gmail.com>
2020-10-04 01:07:26 +0200

    test db changed

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/por964/spa-week40.svg?branch=main)](https://travis-ci.org/por964/spa-week40)
+
+http://claesvonh.surge.sh/
--- target/generated-sources/annotations/entities/ghy636765/Address_.java
+++ target/generated-sources/annotations/entities/ghy636765/Address_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-04T00:46:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-04T01:06:38")
 @StaticMetamodel(Address.class)
 public class Address_ { 
 
